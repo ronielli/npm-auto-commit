2065ffc83783941270614e0a75eea7d138cc4d35
Ronielli <ronielli.jesus@gmail.com>
2023-07-28 22:16:58 -0300

    fix: teste

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,47 @@
 import { createInterface } from 'readline';
 import { writeFileSync, readFileSync } from 'fs';
 
-import { exec } from 'shelljs';
 import { inc } from 'semver';
 
 import Message from './utils/message.util';
 import handleType from './utils/handleType.utils';
 import { green, red, yellow } from './utils/colors.util';
 
+const { execSync } = require('child_process');
+
 const currentDirectory = process.cwd();
 
 export function cli() {
-  if (
-    !exec('git --version', {
-      silent: true,
-      cwd: currentDirectory,
-    }).stdout
-  ) {
+  const gitVersion = execSync('git --version', {
+    silent: true,
+    cwd: currentDirectory,
+  });
+
+  if (!gitVersion) {
     console.log(yellow('O Git não está instalado!'));
     process.exit(1);
   }
-  const user = exec('git config --global user.email', {
+  const user = execSync('git config --global user.email', {
     silent: true,
     cwd: currentDirectory,
-  }).stdout.trim();
+  });
 
   if (!user) {
     console.log(yellow('O Git não está configurado!'));
     process.exit(1);
   }
 
-  if (
-    !exec('git rev-parse --is-inside-work-tree', {
-      silent: true,
-      cwd: currentDirectory,
-    }).stdout
-  ) {
+  const isInitialized = execSync('git rev-parse --is-inside-work-tree', {
+    silent: true,
+    cwd: currentDirectory,
+  });
+
+  if (!isInitialized) {
     console.log(yellow('Nenhum repositório Git foi inicializado!'));
     process.exit(1);
   }
 
-  exec('git pull', { silent: true, cwd: currentDirectory });
+  pull();
 
   const args = process.argv.slice(2);
   if (args.length === 0) {
@@ -77,63 +78,32 @@ export function cli() {
     (arg) =>
       args.includes('-file') || (arg.startsWith('-') && arg.includes('f')),
   );
-  if (add) {
-    exec(`git add .`, { silent: true, cwd: currentDirectory });
-    const status = exec('git diff --cached --name-only | wc -l', {
-      silent: true,
-      cwd: currentDirectory,
-    }).stdout.trim();
 
-    if (status === '0') {
-      console.log(yellow('Nada para comitar!'));
-      process.exit(0);
-    }
-  } else {
-    const status = exec('git diff --cached --name-only | wc -l', {
-      silent: true,
-      cwd: currentDirectory,
-    }).stdout.trim();
-    if (status === '0') {
-      console.log(yellow('Nada para comitar!'));
-      process.exit(0);
-    }
-  }
+  const tagIncrement = args.find(
+    (arg) => arg.startsWith('-') && arg.includes('t'),
+  );
 
+  if (add) execSync(`git add .`, { silent: true, cwd: currentDirectory });
+
+  verifyStatus();
   const message = new Message(description);
 
-  exec('git pull', { silent: true, cwd: currentDirectory });
+  console.log(green('Mensagem de commit:'), message.toString());
 
-  const currentVersion =
-    exec('git describe --tags --abbrev=0 --all', {
-      silent: true,
-      cwd: currentDirectory,
-    })
-      .stdout.trim()
-      .replace(/^tags\//, '') || '0.0.0';
+  const files = listFiles();
+  console.log(green('Arquivos que serão comitados:'), files.join(', '));
 
   const versionType = bigger ? 'major' : handleType(message.getType());
+  const currentVersion = getCurrentTag();
+
   const newVersion = inc(
     currentVersion,
     versionType as 'major' | 'minor' | 'patch',
   );
-
-  if (!newVersion) {
-    console.log(red('O tipo de commit não requer versionamento!'));
-    process.exit(0);
+  if (tagIncrement && newVersion) {
+    console.log(green('Versão atual:'), currentVersion);
+    console.log(green('Nova versão:'), newVersion);
   }
-
-  console.log(green('Mensagem de commit:'), message.toString());
-  console.log(green('Versão atual:'), currentVersion);
-  console.log(green('Nova versão:'), newVersion);
-
-  const files = exec('git diff --cached --name-only', {
-    silent: true,
-    cwd: currentDirectory,
-  }).stdout.trim();
-  console.log(
-    green('Arquivos que serão comitados:'),
-    files.split('\n').join(', '),
-  );
 
   const rl = createInterface({
     input: process.stdin,
@@ -142,29 +112,133 @@ export function cli() {
 
   rl.question('Deseja continuar? (s/n): ', async (answer) => {
     if (answer.toLowerCase() === 's') {
-      if (file) {
-        writeFileSync('./version.txt', newVersion);
-        exec(`git add ./version.txt`, { silent: true, cwd: currentDirectory });
+      if (file && newVersion && tagIncrement) {
+        const json = JSON.stringify({
+          version: newVersion,
+        });
+        writeFileSync('./package.json', json);
+        execSync(`git add ./package.json`, {
+          silent: true,
+          cwd: currentDirectory,
+        });
       } else {
         const json = JSON.parse(readFileSync('./package.json').toString());
         json.version = newVersion;
         writeFileSync('./package.json', JSON.stringify(json, null, 2));
-        exec(`git add ./package.json`, { silent: true, cwd: currentDirectory });
+        execSync(`git add ./package.json`, {
+          silent: true,
+          cwd: currentDirectory,
+        });
       }
 
-      exec(message.toCommit(), {
+      execSync(message.toCommit(), {
         silent: true,
         cwd: currentDirectory,
       });
 
-      exec(`git tag ${newVersion}`, { silent: true, cwd: currentDirectory });
-      exec(`git push`, { silent: true, cwd: currentDirectory });
-      exec(`git push --tags`, { silent: true, cwd: currentDirectory });
+      execSync(`git push`, { silent: true, cwd: currentDirectory });
+      if (tagIncrement && newVersion) {
+        execSync(`git tag -a v${newVersion} -m "${message.toString()}"`, {
+          silent: true,
+          cwd: currentDirectory,
+        });
+        execSync(`git push --tags`, { silent: true, cwd: currentDirectory });
+      }
       console.log(green('Commit realizado com sucesso!'));
     } else {
-      exec(`git reset`, { silent: true, cwd: currentDirectory });
+      execSync(`git reset`, { silent: true, cwd: currentDirectory });
       console.log(yellow('Commit cancelado!'));
     }
     rl.close();
   });
+}
+function getCurrentTag() {
+  try {
+    const gitDescribeOutput = execSync(
+      'git describe --tags $(git rev-list --tags --max-count=1)',
+      {
+        silent: true,
+        cwd: currentDirectory,
+      },
+    );
+
+    const currentVersion = gitDescribeOutput.toString().trim();
+
+    // Check if the output is a valid version tag using a regular expression
+    if (/^v?\d+\.\d+\.\d+/.test(currentVersion)) {
+      return currentVersion.replace(/^v/, ''); // Remove 'v' prefix if present
+    } else {
+      console.log('Invalid version tag found:', currentVersion);
+      return '0.0.0';
+    }
+  } catch (error) {
+    // If an error occurs (e.g., there are no tags), return '0.0.0'
+    console.error('Error while getting current tag:', error);
+    return '0.0.0';
+  }
+}
+
+function verifyStatus() {
+  try {
+    const status = execSync('git diff --cached --name-only | wc -l', {
+      silent: true,
+      cwd: currentDirectory,
+    })
+      .toString()
+      .trim();
+
+    if (status === '0') {
+      console.log(yellow('Nada para comitar!'));
+      process.exit(0);
+    }
+  } catch (error) {
+    console.error('Error while verifying status:', error);
+    process.exit(1); // Exit with an error code if an error occurs
+  }
+}
+
+function listFiles(): string[] {
+  try {
+    // Run 'git diff' command with '--name-status' option
+    const gitDiffOutput = execSync('git diff --cached --name-only', {
+      cwd: currentDirectory,
+    })
+      .toString()
+      .trim();
+    const lines = gitDiffOutput.split('\n');
+
+    // Extract file modifications
+    const fileModifications = lines.map((line: string) => {
+      return line.trim();
+    });
+
+    return fileModifications;
+  } catch (error) {
+    console.error('Error while listing files:', error);
+    return [];
+  }
+}
+
+function pull() {
+  try {
+    // Attempt to run 'git pull' command
+    execSync('git pull', { silent: true, cwd: currentDirectory });
+    console.log(green('Pull OK!'));
+  } catch (error) {
+    // Handle errors from the 'git pull' command
+    console.error('Error while pulling:', error);
+    // Check if there are uncommitted changes
+    const gitStatus = execSync('git status', {
+      silent: true,
+      cwd: currentDirectory,
+    }).toString();
+    if (gitStatus.includes('Changes not staged for commit')) {
+      throw new Error(
+        'You have uncommitted changes. Please commit or stash them before pulling.',
+      );
+    } else {
+      // Other error occurred during 'git pull'
+      throw new Error('Failed to pull from the remote repository.');
+    }
+  }
 }
