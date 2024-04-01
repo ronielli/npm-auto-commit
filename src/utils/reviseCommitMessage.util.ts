3ee78a86db8d972f32919d5fa5e5a15ed26654f6
¨Ronielli¨ <ronielli.jesus@gmail.com>
2024-03-31 21:17:29 -0300

    feat: implementa uso do gpt
    - 4

--- src/utils/reviseCommitMessage.util.ts
+++ src/utils/reviseCommitMessage.util.ts
@@ -21,7 +21,7 @@ async function fetchCommitMessage(commitMessage: string): Promise<string> {
           Authorization: `Bearer ${OPENAI_API_KEY}`,
         },
         body: JSON.stringify({
-          model: 'gpt-3.5-turbo',
+          model: 'gpt-4',
           messages: [
             {
               role: 'system',
